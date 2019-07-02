484c63fb59e79cdfa8af98857c4e7bd037696a76
Daiki-Ishida <missoula0132@gmail.com>
2019-07-02 12:45:26 +0000

    improve design of signup form

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,5 +1,8 @@
 import "init";
 import '@fortawesome/fontawesome-free/js/all';
+
+
 import "components/header/header";
 import "components/top/top";
 import "components/home/home";
+import "components/signup/signup";
