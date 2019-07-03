608e6d948758bece6bb0a82b2341289b91a83853
Daiki-Ishida <missoula0132@gmail.com>
2019-07-03 09:19:44 +0000

    activate validation for user/ferret

--- app/models/ferret.rb
+++ app/models/ferret.rb
@@ -1,3 +1,9 @@
 class Ferret < ApplicationRecord
   belongs_to :user
+
+  # 人間と異なり性別は必須とした。
+  validates :name, presence: true
+  validates :birth_date, presence: true
+  validates :gender, presence: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :ferrets, dependent: :destroy
+
+  # 性別/画像/自己紹介は無しでも登録できる。
+  validates :kanji_lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :kanji_firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :kana_lastname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :kana_firstname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :name, presence: true, length: { in: 1..16 }
+  validates :birth_date, presence: true
+  validates :postal_code, presence: true, length: { is: 7 }
+  validates :postal_address, presence: true
 end
