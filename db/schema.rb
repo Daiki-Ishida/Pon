879797ae775860414085016a7e4434897d0db271
Daiki-Ishida <missoula0132@gmail.com>
2019-07-04 09:05:24 +0000

    create Post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_114449) do
+ActiveRecord::Schema.define(version: 2019_07_04_090031) do
 
   create_table "ferrets", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2019_07_02_114449) do
     t.datetime "updated_at", null: false
     t.date "birth_date"
     t.integer "gender"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
