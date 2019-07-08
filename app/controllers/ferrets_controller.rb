e5dd162689eda608ac2a05de74c930e5f64ccb90
Daiki-Ishida <missoula0132@gmail.com>
2019-07-08 08:01:28 +0000

    improve design of show view

--- app/controllers/ferrets_controller.rb
+++ app/controllers/ferrets_controller.rb
@@ -39,14 +39,14 @@ class FerretsController < ApplicationController
 
     # フェレットから親を先頭に家族の画像、名前を取得。
     # 回りくどい書き方のようにも見える。余裕があれば、他の方法がないか検討。
-    # def family(ferret)
-    #   parent = ferret.user
-    #   siblings = parent.ferrets
-    #   fer_arr = siblings.each_with_object([]) do |s, array|
-    #     array << {image: s.image, name: s.name}
-    #   end
-    #   par_arr = [{image: parent.image, name: parent.name}]
-    #   family_members = fer_arr.unshift(par_arr).flatten
-    #   return family_members
-    # end
+     # def family(ferret)
+     #   owner = ferret.user
+     #   siblings = owner.ferrets.where.not(id: ferret.id)
+     #   fer_arr = siblings.each_with_object([]) do |s, array|
+     #     array << {image: s.image, name: display_name(s), path: ferret_path(s)}
+     #   end
+     #   own_arr = [{image: parent.image, name: display_name(owner)}]
+     #   family_members = fer_arr.unshift(own_arr).flatten
+     #   return family_members
+     # end
 end
