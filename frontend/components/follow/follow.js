085cbb89392847b1a1173be0131f9a81ae673b30
Daiki-Ishida <missoula0132@gmail.com>
2019-07-11 08:08:02 +0000

    improve follow btn

--- frontend/components/follow/follow.js
+++ frontend/components/follow/follow.js
@@ -2,3 +2,54 @@ import "./follow.css";
 
 import "../follow-btn/follow-btn";
 import "../unfollow-btn/unfollow-btn";
+
+function followSwitch(e){
+  e.preventDefault();
+  e.currentTarget.disabled = true;
+  const id = e.target.value;
+  const name = e.target.name;
+  const oldBtn = e.currentTarget;
+  const elm = document.getElementById(`follow_${id}`)
+  let url = '';
+  let sendData = '';
+  let htmlMethod = '';
+  if(name === 'follow'){
+    url = 'http://localhost:3000/relationships';
+    sendData = {
+      follow: {
+        followed_id: id
+      }
+    };
+    htmlMethod = 'POST'
+  }else if(name === 'unfollow'){
+    url = `http://localhost:3000/relationships/${id}`;
+    htmlMethod = 'DELETE';
+  }else{
+    alert('ERROR!')
+  }
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log('success!');
+      const newBtn = this.response;
+      elm.textContent = '';
+      elm.insertAdjacentHTML('beforeend', newBtn);
+    } else if (this.readyState == 4) {
+      alert('ERROR!');
+      e.currentTarget.disabled = false;
+    }
+  }
+  const csfr = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  xhr.open(htmlMethod, url);
+  xhr.setRequestHeader('X-CSRF-Token', csfr);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(JSON.stringify(sendData));
+}
+
+window.onload = function(){
+  const follows = document.getElementsByClassName('follow');
+  for(var i = 0; i < follows.length; i++){
+    var follow = follows[i];
+    follow.addEventListener('click', followSwitch, false);
+  }
+}
