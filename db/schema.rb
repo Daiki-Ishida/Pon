bda8ee221adb834d87cde7f759e0e1b43ce3c97a
Daiki-Ishida <missoula0132@gmail.com>
2019-07-11 09:42:21 +0000

    create Room/Message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_084025) do
+ActiveRecord::Schema.define(version: 2019_07_11_093614) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2019_07_07_084025) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "room_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -51,6 +59,13 @@ ActiveRecord::Schema.define(version: 2019_07_07_084025) do
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "owner_id"
+    t.integer "guest_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
