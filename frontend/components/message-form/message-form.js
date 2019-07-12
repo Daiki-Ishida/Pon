7739411e8af1eea1d5a62c87d7c0664b57f5122f
Daiki-Ishida <missoula0132@gmail.com>
2019-07-12 14:52:59 +0000

    improve design of message form

--- frontend/components/message-form/message-form.js
+++ frontend/components/message-form/message-form.js
@@ -6,12 +6,25 @@ window.onload = function(){
   const intViewportHeight = window.innerHeight;
   const minLineHeight = heightOfLine * 1.5;
   const maxLineHeight = intViewportHeight * 0.5;
-
-  const resizeForm = (event)=>{
+  const messageFormBtn = document.getElementById('messageFormBtn');
+  if(messageFormBtn != null){
+    messageFormBtn.disabled = true;
+  }
+  const resizeForm = ()=>{
     const lines = (messageFormContent.value + '\n').match(/\n/g).length;
-    console.log(lines);
     const num = Math.min(maxLineHeight, Math.max(heightOfLine * lines, minLineHeight));
     messageFormContent.style.height = num + 'px';
+
+    if(messageFormContent.value != '' && lines == 1){
+      messageFormBtn.disabled = false;
+      messageFormContent.style.borderTopRightRadius = '0';
+    }else if(messageFormContent.value != '' && lines > 1){
+      messageFormContent.style.borderTopRightRadius = '5px';
+      messageFormBtn.disabled = false;
+    }else{
+      messageFormBtn.disabled = true;
+    }
+
   }
   if(messageFormContent != null){
     messageFormContent.addEventListener('input', resizeForm);
