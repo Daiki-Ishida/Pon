a9591bd498e6f49f7fb8a764c698990f9b709d60
Daiki-Ishida <missoula0132@gmail.com>
2019-07-12 13:34:36 +0000

    improve message form

--- frontend/components/message-form/message-form.js
+++ frontend/components/message-form/message-form.js
@@ -1,1 +1,19 @@
 import "./message-form.css";
+
+window.onload = function(){
+  const messageFormContent = document.getElementById('messageFormContent');
+  const heightOfLine = getComputedStyle(messageFormContent).getPropertyValue('line-height').split('px')[0];
+  const intViewportHeight = window.innerHeight;
+  const minLineHeight = heightOfLine * 1.5;
+  const maxLineHeight = intViewportHeight * 0.5;
+
+  const resizeForm = (event)=>{
+    const lines = (messageFormContent.value + '\n').match(/\n/g).length;
+    console.log(lines);
+    const num = Math.min(maxLineHeight, Math.max(heightOfLine * lines, minLineHeight));
+    messageFormContent.style.height = num + 'px';
+  }
+  if(messageFormContent != null){
+    messageFormContent.addEventListener('input', resizeForm);
+  }
+}
