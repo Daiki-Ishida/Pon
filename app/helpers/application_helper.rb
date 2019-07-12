d51cd2ea254ce8204717b7b701c9fbe3c5e7d26f
Daiki-Ishida <missoula0132@gmail.com>
2019-07-12 08:47:00 +0000

    improve design of message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,6 +56,10 @@ module ApplicationHelper
     end
   end
 
+  def display_time(time)
+    time.strftime("%Y-%m-%d %H:%M")
+  end
+
   def current_user
     if session[:user_id]
       current_user ||= User.find_by(id: session[:user_id])
