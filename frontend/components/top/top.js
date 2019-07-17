429e9a22782e1f7a8ea1033cdedb5ec8844527b5
Daiki-Ishida <missoula0132@gmail.com>
2019-07-17 10:33:57 +0000

    add swiper on top page

--- frontend/components/top/top.js
+++ frontend/components/top/top.js
@@ -1,5 +1,41 @@
+import "./swiper.min.css";
 import "./top.css";
+
 import "../catch/catch";
 import "../description/description";
 import "../owner-steps/owner-steps";
 import "../sitter-steps/sitter-steps";
+
+
+import Swiper from 'swiper';
+
+const mainSlider = new Swiper('.swiper-container',{
+  direction: 'vertical',
+  speed: 1000,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className){
+      let str = '';
+      switch (index){
+        case 0:
+          str = 'TOP'
+          break;
+        case 1:
+          str = "PONとは？"
+          break;
+        case 2:
+          str = '~預け方~'
+          break;
+        case 3:
+          str = '~預かり方~'
+          break;
+        case 4:
+          str = '早速始めよう!'
+          break;
+        }
+        return '<span class="' + className + '">' + str + '</span>';
+      }
+    }
+  });
