4885497d50281685952cb392c684ade8c3859021
Daiki-Ishida <missoula0132@gmail.com>
2019-07-26 06:09:14 +0000

    imrove input form for territory

--- frontend/components/territory/territory.js
+++ frontend/components/territory/territory.js
@@ -1,20 +1,21 @@
 import "./territory.css";
 
-
 const territoryMap = document.getElementById('map')
 
 const initMap = ()=>{
   const center = document.getElementById('center').value;
   const radiusInKms = document.getElementById('radius').value;
   let zoom = '';
-  if(radiusInKms == 5){
+  if(radiusInKms <= 5){
     zoom = 12;
-  }else if(radiusInKms == 10){
+  }else if(radiusInKms <= 10){
     zoom = 11;
-  }else if(radiusInKms == 15){
+  }else if(radiusInKms <= 15){
     zoom = 10.5;
-  }else if(radiusInKms == 20){
+  }else if(radiusInKms <= 20){
     zoom = 10;
+  }else if(radiusInKms > 20){
+    zoom = 9;
   }
   let map = new google.maps.Map(territoryMap, {
     zoom: zoom,
@@ -42,4 +43,14 @@ const initMap = ()=>{
 
 if(territoryMap != null){
   initMap();
+}
+
+const territoryRange = document.getElementById('territoryRange');
+const rangeDisplay = document.getElementById('rangeDisplay');
+
+if(territoryRange){
+  rangeDisplay.innerHTML = territoryRange.value;
+  territoryRange.addEventListener('input', function(e){
+    rangeDisplay.innerHTML = e.target.value;
+  });
 }
