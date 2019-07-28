e1e7c9b80ffd98b3398bbe31a029789de78e2724
Daiki-Ishida <missoula0132@gmail.com>
2019-07-28 10:55:30 +0000

    modify for deploy to aws

--- db/migrate/20190707084025_add_default_pic.rb
+++ db/migrate/20190707084025_add_default_pic.rb
@@ -1,7 +1,7 @@
 class AddDefaultPic < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :image, :text, default: "no_image.jpg"
-    change_column :ferrets, :image, :text, default: "no_image.jpg"
-    change_column :posts, :image, :string, default: "no_image.jpg"
+    # change_column :users, :image, :text, default: "no_image.jpg"
+    # change_column :ferrets, :image, :text, default: "no_image.jpg"
+    # change_column :posts, :image, :string, default: "no_image.jpg"
   end
 end
