679ee8814a344868c400d25596bd8cbd9cd5c665
Daiki-Ishida <missoula0132@gmail.com>
2019-08-03 04:32:02 +0000

    complete test for like model

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @like = likes(:one)
+  end
+
+  test "user_id should be present" do
+    @like.user_id = nil
+    assert_not @like.valid?
+  end
+
+  test "post_id should be present" do
+    @like.post_id = nil
+    assert_not @like.valid?
+  end
 end
