2db8787a1fe3b812d96cae7a75264bba30830e98
Daiki-Ishida <missoula0132@gmail.com>
2019-08-03 04:58:17 +0000

    complete test for message model

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @room = rooms(:one)
+    @message = Message.create(
+      sender_id: @user.id,
+      room_id: @room.id,
+      content: "テストだよ"
+    )
+  end
+
+  test "sender_id should be present" do
+    @message.sender_id = nil
+    assert_not @message.valid?
+  end
+
+  test "room_id should be present" do
+    @message.room_id = nil
+    assert_not @message.valid?
+  end
+
+  # create_notification(user, action)メソッド
+  test "should create notification" do
+    assert_difference 'Notification.count', 1 do
+      @message.create_notification(@user, @room)
+    end
+  end
 end
