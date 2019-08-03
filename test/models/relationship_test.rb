3ae781dde4d2c7fbb753f846beba9a8cbd3a33ff
Daiki-Ishida <missoula0132@gmail.com>
2019-08-03 04:34:33 +0000

    complete test for relationshop model

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = relationships(:one)
+  end
+
+  test "follower_id should be present" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "followed_id should be present" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
