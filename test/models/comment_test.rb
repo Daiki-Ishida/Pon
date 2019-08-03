01e8e7656f7a9ae1ca446fdb335393721306934f
Daiki-Ishida <missoula0132@gmail.com>
2019-08-03 04:45:24 +0000

    complete test for comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = comments(:one)
+  end
+
+  test "content should be present" do
+    @comment.content = "   "
+    assert_not @comment.valid?
+  end
+
+  test "user_id should be present" do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "post_id should be present" do
+    @comment.post_id = nil
+    assert_not @comment.valid?    
+  end
 end
