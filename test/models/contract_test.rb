bd983272a8d96107b6f07e8c19afd1112537ef9b
Daiki-Ishida <missoula0132@gmail.com>
2019-08-03 05:29:10 +0000

    complete test for contract model

--- test/models/contract_test.rb
+++ test/models/contract_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ContractTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @contract = contracts(:one)
+  end
+
+  test "owner_id should be present" do
+    @contract.owner_id = nil
+    assert_not @contract.valid?
+  end
+
+  test "sitter_id should be present" do
+    @contract.sitter_id = nil
+    assert_not @contract.valid?
+  end
+
+  test "start_at should be present" do
+    @contract.start_at = "   "
+    assert_not @contract.valid?
+  end
+
+  test "end_at should be present" do
+    @contract.end_at = "   "
+    assert_not @contract.valid?
+  end
+
+  test "fee should be present" do
+    @contract.fee = nil
+    assert_not @contract.valid?
+  end
+
+  test "memo should be present" do
+    @contract.memo = nil
+    assert_not @contract.valid?
+  end
 end
