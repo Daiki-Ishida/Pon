cb94f14cb394685dc9e7a31b2d45f5d774ab762c
Daiki-Ishida <missoula0132@gmail.com>
2019-08-03 05:05:47 +0000

    complete test for room model

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @room = rooms(:one)
+    @user = users(:one)
+    @opponent_user = users(:two)
+  end
+
+  test "owner_id should be present" do
+    @room.owner_id = nil
+    assert_not @room.valid?
+  end
+
+  test "guest_id should be present" do
+    @room.guest_id = nil
+    assert_not @room.valid?
+  end
+
+  # Opponentメソッド
+  test "should return opponent user" do
+    assert @room.opponent(@user) == @opponent_user
+  end
+
+  # find roomメソッド
+  test "should find room from user id" do
+    assert Room.find_room(@user.id, @opponent_user.id) == @room
+  end
 end
